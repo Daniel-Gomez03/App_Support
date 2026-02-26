c7efb7c4a5d34994a96017381e7cecfcd32b608e
Daniel-Gomez03 <danielitogomez99@gmail.com>
2026-02-26 08:58:07 -0600

    fix: modulo de crear ticket terminedo y responsive

--- frontend/src/pages/CreateTicket.jsx
+++ frontend/src/pages/CreateTicket.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styles from './CreateTicket.module.less';
-import { LuUpload } from "react-icons/lu";
+import { LuUpload, LuCheck, LuX } from "react-icons/lu";
+import { FaQuestion } from "react-icons/fa"
+import "flag-icons/css/flag-icons.min.css";
 
 const CreateTicket = () => {
 
@@ -10,6 +12,7 @@ const CreateTicket = () => {
     invoiceNumber: '',
     email: '',
     phone: '',
+    countryCode: '+504',
     subject: '',
     category: '',
     deviceType: '',
@@ -18,6 +21,38 @@ const CreateTicket = () => {
     description: '',
     files: null
   });
+
+  const handleCancel = () => {
+
+    setFormData({
+      clientName: '',
+      company: '',
+      invoiceNumber: '',
+      email: '',
+      phone: '',
+      countryCode: '+504',
+      subject: '',
+      category: '',
+      deviceType: '',
+      model: '',
+      serialNumber: '',
+      description: '',
+      files: null
+    });
+  };
+
+  const countries = [
+    { code: '+505', iso: 'ni', name: 'Nicaragua' },
+    { code: '+504', iso: 'hn', name: 'Honduras' },
+    { code: '+503', iso: 'sv', name: 'EL Salvador' },
+    { code: '+502', iso: 'gt', name: 'Guatemala' },
+  ];
+
+  const currentFlag = countries.find(c => c.code === formData.countryCode)?.iso || 'hn';
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [showSuccess, setShowSuccess] = useState(false);
 
   useEffect(() => {
     document.title = "Soporte | Crear Ticket";
@@ -38,19 +73,20 @@ const CreateTicket = () => {
     }));
   };
 
-  const handleSubmit = async (e) => {
+  const handleReview = (e) => {
     e.preventDefault();
+    setIsModalOpen(true);
+  };
 
-    const dataToSend = new FormData();
-    Object.keys(formData).forEach(key => {
-      dataToSend.append(key, formData[key]);
-    });
+  const handleFinalSubmit = () => {
+    console.log("--- ENVIANDO A LARAVEL ---", formData);
+    setIsModalOpen(false);
 
-    console.log("--- DATOS LISTOS PARA ENVIAR (POST) ---");
-    for (let [key, value] of dataToSend.entries()) {
-      console.log(`${key}:`, value);
-    }
-    alert("Revisa la consola (F12) para ver los datos capturados.");
+    setShowSuccess(true);
+
+    setTimeout(() => {
+      setShowSuccess(false);
+    }, 5000);
   };
 
   return (
@@ -60,7 +96,7 @@ const CreateTicket = () => {
         <p className={styles.subtitle}>Ingresa la información detallada para registrar el caso.</p>
       </div>
 
-      <form onSubmit={handleSubmit} className={styles.formCard}>
+      <form onSubmit={handleReview} className={styles.formCard}>
 
         <div className={styles.section}>
           <div className={styles.sectionHeader}>
@@ -113,12 +149,34 @@ const CreateTicket = () => {
             </div>
             <div className={styles.inputGroup}>
               <label>Teléfono</label>
-              <input
-                type="tel"
-                name='phone'
-                value={formData.phone}
-                onChange={handleChange}
-                placeholder="+504 9999-9999" />
+              <div className={styles.phoneContainer}>
+                <div className={styles.flagWrapper}>
+                  <span className={`fi fi-${currentFlag}`}></span>
+                </div>
+
+                <select
+                  name="countryCode"
+                  value={formData.countryCode}
+                  onChange={handleChange}
+                  className={styles.countrySelect}
+                >
+                  {countries.map((country) => (
+                    <option key={country.code} value={country.code}>
+                      ({country.name})
+                    </option>
+                  ))}
+                </select>
+
+                <div className={styles.divider}></div>
+                <input
+                  type="tel"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  placeholder="9702-9226"
+                  className={styles.phoneInput}
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -163,7 +221,7 @@ const CreateTicket = () => {
                 name='deviceType'
                 value={formData.deviceType}
                 onChange={handleChange}
-                placeholder=""
+                placeholder="Preguntar que va aqui"
                 required />
             </div>
           </div>
@@ -176,17 +234,18 @@ const CreateTicket = () => {
                 name='model'
                 value={formData.model}
                 onChange={handleChange}
-                placeholder=""
+                placeholder="Preguntar que va aqui"
                 required />
             </div>
             <div className={styles.inputGroup}>
               <label>No. de Serie <span className={styles.required}>*</span></label>
-              <input 
+              <input
                 type="text"
                 name='serialNumber'
                 value={formData.serialNumber}
-                placeholder="Ingrese serie para verificar garantía" 
-                required/>
+                onChange={handleChange}
+                placeholder="Ingrese serie para verificar garantía"
+                required />
             </div>
           </div>
         </div>
@@ -199,10 +258,11 @@ const CreateTicket = () => {
 
           <div className={styles.inputGroup}>
             <label>Descripción del Problema <span className={styles.required}>*</span></label>
-            <textarea 
-              rows="4" 
+            <textarea
+              rows="4"
               name='description'
               value={formData.description}
+              onChange={handleChange}
               placeholder="Describa detalladamente la falla reportada..."
               required></textarea>
           </div>
@@ -210,10 +270,15 @@ const CreateTicket = () => {
           <div className={styles.inputGroup}>
             <label>Adjuntar Evidencia <span className={styles.required}>*</span></label>
             <div className={styles.uploadArea}>
-              <input type="text" />
+              <input
+                type="file"
+                onChange={handleFileChange}
+                style={{ display: 'none' }} />
               <div className={styles.uploadContent}>
                 <LuUpload className={styles.luUpload} />
-                <span className={styles.uploadText}>Haga clic o arrastre archivos aquí</span>
+                <span className={styles.uploadText}>
+                  {formData.files ? formData.files.name : "Haga clic o arrastre archivos aquí"}
+                </span>
                 <span className={styles.uploadHint}>Soporta: JPG, PNG, PDF, MP4 (Max 15MB)</span>
               </div>
             </div>
@@ -221,14 +286,60 @@ const CreateTicket = () => {
         </div>
 
         <div className={styles.formActions}>
-          <button className={styles.btnCancel}>Cancelar</button>
-          <button className={styles.btnSubmit}>Crear Ticket</button>
+          <button
+            type="button"
+            className={styles.btnCancel}
+            onClick={handleCancel}>
+            Cancelar
+          </button>
+          <button type="submit" className={styles.btnSubmit}>Crear Ticket</button>
         </div>
-
       </form>
-      <div className=''>
 
-      </div>
+      {isModalOpen && (
+        <div className={styles.modalOverlay}>
+          <div className={styles.confirmationCard}>
+
+            <div className={styles.iconWrapper}>
+              <FaQuestion className={styles.faIcon} />
+            </div>
+
+            <h2 className={styles.modalTitle}>¿Seguro que deseas crear el ticket?</h2>
+
+            <p className={styles.modalText}>
+              Al confirmar, el ticket se registrará y no podrá modificarse.
+            </p>
+            <p className={styles.modalText}>
+              El Ticket será gestionado por un administrador para poder avanzar
+            </p>
+
+            <div className={styles.modalButtons}>
+              <button onClick={() => setIsModalOpen(false)} className={styles.btnCancelModal}>
+                Cancelar
+              </button>
+              <button onClick={handleFinalSubmit} className={styles.btnAcceptModal}>
+                Aceptar
+              </button>
+            </div>
+
+          </div>
+        </div>
+      )}
+
+      {showSuccess && (
+        <div className={styles.successToast}>
+          <div className={styles.toastIcon}>
+            <LuCheck className={styles.checkIcon} />
+          </div>
+          <div className={styles.toastContent}>
+            <h4>El Ticket se ha creado exitosamente</h4>
+            <p>Ve a Asignar Ticket para poder gestionarlo y asignar colaboradores</p>
+          </div>
+          <button onClick={() => setShowSuccess(false)} className={styles.toastClose}>
+            <LuX />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
