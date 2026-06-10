4f5d6908ebf34c97c4831e81f38321b1fe2df866
Daniel-Gomez03 <danielitogomez99@gmail.com>
2026-06-09 22:31:41 -0600

    feat: prueba para generar el apk

--- mobile/App_support/Services/qaService.ts
+++ mobile/App_support/Services/qaService.ts
@@ -6,7 +6,7 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-const API_URL = 'http://192.168.1.20:8000/api';
+const API_URL = 'http://192.168.1.24:8000/api';
 
 const authHeader = async () => {
     const token = await SecureStore.getItemAsync('userToken');
--- mobile/App_support/Services/socket.ts
+++ mobile/App_support/Services/socket.ts
@@ -8,7 +8,7 @@
 
 import { io } from "socket.io-client";
 
-const SOCKET_URL = 'http://192.168.1.20:8000';
+const SOCKET_URL = 'http://192.168.1.24:8000';
 
 const socket = io(SOCKET_URL, {
   transports: ["websocket"],
